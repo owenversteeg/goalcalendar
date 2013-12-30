0c78b18473c0ebe993f184e6047fe5413ed0ed55
Owen Versteeg <owen.versteeg@live.com>
2013-12-30 12:49:31 -0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+//in the format date:descriptiontext
+
+var violin = {
+	'name': 'Violin',
+	'color': 'blue',
+	'4/11/2013': '08:00 Data',
+	'4/14/2013': '17:30 Data',
+	'4/15/2013': '17:00 Data',
+	'4/21/2013': '17:30 Data',
+	'4/22/2013': '17:00 Data',
+	'4/24/2013': '17:00 Data',
+	'4/26/2013': '17:00 Data',
+	'4/27/2013': '17:00 Data'
+};
+
+var school = {
+	'name': 'School',
+	'color': 'red',
+	'4/11/2013': '01:00 Data',
+	'4/12/2013': '02:00 Data',
+	'4/14/2013': '20:00 Data',
+	'4/31/2013': '15:00 Data'
+};
+
+var datas = [];
+datas[0] = violin;
+datas[1] = school;
