2164c01509596d6edb10ca2c69754a557b70c306
Owen Versteeg <owen.versteeg@live.com>
2014-01-02 23:58:31 -0500

    add more events, change time and date

--- data.js
+++ data.js
@@ -1,27 +1,47 @@
-//in the format date:descriptiontext
-
 var violin = {
 	'name': 'Violin',
 	'color': 'blue',
-	'4/11/2013': '08:00 Data',
-	'4/14/2013': '17:30 Data',
-	'4/15/2013': '17:00 Data',
-	'4/21/2013': '17:30 Data',
-	'4/22/2013': '17:00 Data',
-	'4/24/2013': '17:00 Data',
-	'4/26/2013': '17:00 Data',
-	'4/27/2013': '17:00 Data'
+	'1/11/2014': '08:00 Practice the Vivaldi concerto and the Brahms.',
+	'1/14/2014': '17:30 Do vibrato exercises for 30 minutes.',
+	'1/15/2014': '17:00 Work out fingerings for the new orchestra piece.',
+	'1/21/2014': '17:30 Replace E string, practice shifting.',
+	'1/22/2014': '17:00 Work on dynamics and tempo for the Tchaikovsky.',
+	'1/24/2014': '17:00 Find a duet piece for the Vivaldi.',
+	'1/26/2014': '17:00 Practice the Brahms with piano and viola accompaniment.',
+	'1/27/2014': '17:00 Purchase a new bow and rosin.'
 };
 
 var school = {
 	'name': 'School',
 	'color': 'red',
-	'4/11/2013': '01:00 Data',
-	'4/12/2013': '02:00 Data',
-	'4/14/2013': '20:00 Data',
-	'4/31/2013': '15:00 Data'
+	'1/11/2014': '09:00 Study for the Chemistry final',
+	'1/12/2014': '10:00 Practice Spanish vocabulary words',
+	'1/14/2014': '20:00 Study for the English final',
+	'1/31/2014': '15:00 Re-read Beowulf'
 };
+
+var rowing = {
+	'name': 'Rowing',
+	'color': 'yellow',
+	'1/4/2014': 'Set up ergs in the gym',
+	'1/6/2014': 'Practice on erg for 90 minutes',
+	'1/15/2014': 'Practice on erg for 60 minutes, practice 2k',
+	'1/24/2014': 'Practice on erg for 70 minutes, purchase new gloves'
+}
+
+var cleaning = {
+	'name': 'Cleaning',
+	'color': 'green',
+	'1/2/2014': 'Dust the pantry and the shelves',
+	'1/5/2014': 'Clean up needles from Christmas tree',
+	'1/14/2014': 'Move junk out of living room',
+	'1/17/2014': 'Organize books on bookshelf',
+	'1/24/2014': 'Wash the sheets and do the dry cleaning',
+	'1/29/2014': 'Mop the living room floor'
+}
 
 var datas = [];
 datas[0] = violin;
 datas[1] = school;
+datas[2] = rowing;
+datas[3] = cleaning;
