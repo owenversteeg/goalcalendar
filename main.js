fe92fd9e08a9fea3c6ab6c3c822a27801630b88e
Owen Versteeg <owen.versteeg@live.com>
2014-01-04 18:41:26 -0500

    save data types

--- main.js
+++ main.js
@@ -86,6 +86,7 @@ $(document).ready(function() {
 	if (localStorage.getItem('username') && localStorage.getItem('key')) {
 		document.head.innerHTML += "<style type='text/css'>.hero { display: block; }</style>"
 		makeLoggedInStyle(localStorage.getItem('username'));
+		getNewDataFile();
 	}
 	
 	updateGoalText();
